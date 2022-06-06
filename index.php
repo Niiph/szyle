26abc7f95e749f0886dd76adf9b27ca51801feed
Niiph <pepo.o@wp.pl>
2022-06-06 02:25:39 +0200

    some fixes

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <html lang="pl">
-    <head>
-        <title>Kalkulator</title>
-        <link rel="Stylesheet" type="text/css" href="style.css"/>
-    </head>
-    <body>
-        <div>
-            <div class="calculate">
+<head>
+    <title>Kalkulator</title>
+    <link rel="Stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<div>
+    <div class="calculate">
         <p>Podaj wartości wyrażone w centymetrach:</p>
         <form method="POST" action="valid.php">
-            <span class="input_description">wysokość:</span> <input type="number" name="height"><br>
-            <span class="input_description">szerokość:</span> <input type="number" name="weight"><br>
-            <span class="input_description">długość:</span> <input type="number" name="lenght"><br>
+            <span class="input_description">wysokość:</span> <input type="number" name="height" required><br>
+            <span class="input_description">szerokość:</span> <input type="number" name="weight" required><br>
+            <span class="input_description">długość:</span> <input type="number" name="length" required><br>
             <input class="button" type="submit">
-        </form></div></div>
-    </body>
+        </form>
+    </div>
+</div>
+</body>
 </html>
--- valid.php
+++ valid.php
@@ -1,20 +1,15 @@
 <?php
+    $weight = $_POST['weight'] ?? null;
+    $length = $_POST['length'] ?? null;
+    $height = $_POST['height'] ?? null;
 
-if (isset($_POST['weight'])) {
-    $weight = $_POST['weight'];
+if (!$weight || !$length || !$height) {
+    echo 'Błąd! Nie wszystkie dane zostały przesłane.';
+    exit;
 }
 
-
-if (isset($_POST['lenght'])) {
-    $lenght = $_POST['lenght'];
-}
-
-if (isset($_POST['height'])) {
-    $height = $_POST['height'];
-}
-
-if (strlen($weight) > 1 && strlen($height) > 1 && strlen($lenght) > 1) {
-    $result = (int) $weight * (int) $height * (int) $lenght;
+if (strlen($weight) > 1 && strlen($height) > 1 && strlen($length) > 1) {
+    $result = (int)$weight * (int)$height * (int)$length;
     if (($result / 112000) >= 1) {
         echo 'Twoja klatka pomieści ';
         $cage = round($result / 112000);
